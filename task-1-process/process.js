aaa9576a2d03e48f9a1da434111d2d52ceb5ec55
Maryna <maryny.khrystyianyn@gmail.com>
2025-04-10 16:08:55 +0200

    task1

--- task-1-process/process.js
+++ task-1-process/process.js
@@ -1,4 +1,12 @@
 import fs from 'fs'
 
-const path = "/"
-const content = fs.readdirSync(path)
+// const path = "/"
+const path = process.env.HOME;// alles funkcionirt aber content ist andere
+const content = fs.readdirSync(path);
+console.log(content);
+console.log("Platform - ",process.platform);
+console.log("argv - ",process.argv);
+console.log("PWD - ",process.env.PWD);
+console.log("HOME - ",process.env.HOME);
+console.log("USER - ",process.env.USER);
+console.log("FOO - ",process.env.FOO);
